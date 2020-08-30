47c609dd19c2449803e4df0c628cfefe067602da
Youssouf Kacemi <yousskacemi1995@gmail.com>
2020-08-30 15:34:27 +0100

    finishing the style of the responsive sidebar.

--- resources/js/components/template/sidebar.vue
+++ resources/js/components/template/sidebar.vue
@@ -78,6 +78,45 @@
 					            </div>
 	        			</li>
         			</a>
+
+        			<li style="list-style: none;">
+		        				<div style="margin: 6% auto;">
+					        		<div>
+					        			<button id="post" class="btn btn-success btn-sidebar bi bi-lightning-fill ">
+					        				<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-lightning-fill text-light" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+											  <path fill-rule="evenodd" d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
+											</svg>
+					        				<span class="text-button">Post</span>
+					        			</button>
+					        		</div>
+					        		<div>
+					        			<button id="report" class="btn btn-danger btn-sidebar">
+					        				<svg width="1.5625em" height="1.5em" viewBox="0 0 17 16" class="bi bi-exclamation-triangle-fill text-light" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+											  <path fill-rule="evenodd" d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 5zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+											</svg>
+					        				<span class="text-button">Report</span>
+					        			</button>
+					        		</div>  
+					        	</div>
+	        			</li>
+
+
+	        			<li class="profile-bottom">
+		        				<button class="btn" style="width:100%">
+						        	<div class="profile-sidebar">
+						        		<div class="profile-link-right-side profile-ticket">
+					  						<img class="img-md img-bottom"  src="/images/profile.jpg" alt="">
+					  						<div>
+					  							<h6 class="header-logo-text-sidebar-link" style="font-weight: bold;">Youssouf Kacemi</h6>
+					  							<span class="username" style="float: left; color: #666;">@el.dritch</span>
+						        			</div>
+						        		</div>
+						        		<div class="up-arrow-part" style="margin-right: 6%;">
+						        			<img style="width: 16px;height: 16px;" src="/images/side-menu-icons/arrow.svg">
+						        		</div>
+						        	</div>
+					        	</button>
+	        			</li>
         			<!-- <li class="menu-item">
         				<a class="menu-item-link" href="">
 	        				<div class="menu-item-container">
@@ -88,28 +127,9 @@
         			</li> -->
         		</ul>
         	</div>
-        	<div style="margin: 6% auto;">
-        		<div>
-        			<button class="btn btn-success btn-sidebar">Post an Ideas</button>
-        		</div>
-        		<div>
-        			<button class="btn btn-danger btn-sidebar">Report Something</button>
-        		</div>        		
-        	</div>
-        	<button class="btn" style="width:100%">
-	        	<div class="profile-sidebar">
-	        		<div class="profile-link-right-side">
-  						<img class="img-md"  src="/images/profile.jpg" alt="">
-  						<div>
-  							<h6 class="header-logo-text-sidebar-link" style="font-weight: bold;">Youssouf Kacemi</h6>
-  							<span style="float: left; color: #666;">@el.dritch</span>
-	        			</div>
-	        		</div>
-	        		<div style="margin-right: 6%;">
-	        			<img style="width: 16px;height: 16px;" src="/images/side-menu-icons/arrow.svg">
-	        		</div>
-	        	</div>
-        	</button>
+        	
+
+        	
 
 
 
--- resources/js/components/template/wall.vue
+++ resources/js/components/template/wall.vue
@@ -1,15 +1,6 @@
 <template>
 	<div class="container-fluid main-wall">
-		<div style="padding: 10px 15px;
-    border-bottom: 1px solid rgb(238, 238, 238);
-    position: fixed;
-    top: 0;
-    z-index: 3333;
-    background: #15202B;
-    right: 350px;
-    left: 350px;
-    margin-left: 16px;
-    width: 619px;">
+		<div class="main-wall-header">
 			<h1 style="font-weight: bold; 
     letter-spacing: 3px;">Ideas</h1>
 		</div>
